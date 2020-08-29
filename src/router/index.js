7020aa7c9636f9ebc0828ee11f6e79712df7487e
ethz123 <1098620570@qq.com>
2020-08-29 10:54:42 +0800

    完成了Report功能模块的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -58,7 +59,14 @@ const routes = [{
         path: '/goods/add',
         component: Add
       },
-      { path: '/orders', component: Order }
+      {
+        path: '/orders',
+        component: Order
+      },
+      {
+        path: '/reports',
+        component: Report
+      }
     ]
   }
 ]
